fd47e576e9a7b878621bf735c75a916dd7b3c78b
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2022-10-27 22:19:56 +0900

    Js working

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const game = () => {
   let pScore = 0;
   let cScore = 0;
 
+  //Start the Game
   const startGame = () => {
     const playBtn = document.querySelector(".intro button");
     const introScreen = document.querySelector(".intro");
     const match = document.querySelector(".match");
 
-    playBtn.addEventListener("click", function () {
+    playBtn.addEventListener("click", () => {
       introScreen.classList.add("fadeOut");
       match.classList.add("fadeIn");
     });
   };
 
+  // Play match
+  const playMatch = () => {
+    const options = document.querySelectorAll('.options button');
+  };
   // Is call all the inner function
   startGame();
-
+  console.log("fadeIn");
 };
 
 // start the game function
